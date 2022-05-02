8fde10c6ac6ae9d71c73fa96cc7f4823cdc2f53c
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-02 13:26:29 -0400

    added javascript

--- js/script.js
+++ js/script.js
@@ -8,12 +8,33 @@
  * Check service worker.
  */
  if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS20-PWA-Template/sw.js", {
-    scope: "/ICS20-PWA-Template/",
+  navigator.serviceWorker.register("/ICS2O-UNIT-5-04-HTML/sw.js", {
+    scope: "/ICS2O-UNIT-5-04-HTML/",
   })
 }
 
 /**
  * Input.
  */
+
+ function buttonClicked() {
+
+  const age = parseInt(document.getElementById('age').value)
+
+  var monday = document.getElementById("monday")
+  var tuesday = document.getElementById("tuesday")
+  var wednesday = document.getElementById("wednesday")
+  var thursday = document.getElementById("thursday")
+  var friday = document.getElementById("friday")
+  var sunday = document.getElementById("sunday")
+  var saturday = document.getElementById("saturday")
+
+  if ((tuesday.checked == true || thursday.checked == true) || (age >= 12 && age <= 21)) {
+    document.getElementById("output").innerHTML =
+  "You are eligible for student pricing!"
+  } else {
+  document.getElementById("output").innerHTML =
+  "You must pay regular pricing."
+  }
+ }
 
